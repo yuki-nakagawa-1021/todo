75b10f7e1c8eef3e7a91772d024190646a42b7ae
nakagawa yuki <mstyk1021@gmail.com>
2026-01-15 00:39:16 +0900

    　テーブル作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController
+use App\Http\Controllers\TodoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
