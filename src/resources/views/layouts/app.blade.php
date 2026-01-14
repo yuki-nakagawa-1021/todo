b41264f36aad1289142596b82700db4c56a107a4
nakagawa yuki <mstyk1021@gmail.com>
2026-01-15 01:46:24 +0900

    　ビュー修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo</title>
-    <link rel="stylesheet" href="{{ asset('css/sanitize.css) }}" />
-    <link rel="stylesheet" href="{{ asset('css/common.css) }}" />
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css’) }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css’) }}" />
     @yield('css')
 </head>
 
